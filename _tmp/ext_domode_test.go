33f7aca425f2677d72098a1f7f232a752501c018
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-04 15:50:56 -0600

    Cleanup

--- _tmp/ext_domode_test.go
+++ _tmp/ext_domode_test.go
@@ -3,12 +3,12 @@ package main_test
 import (
 	"github.com/gonyyi/alog"
 	"github.com/gonyyi/alog/ext"
-  "os"
-  "testing"
+	"os"
+	"testing"
 )
 
-func TestMode(t *testing.T) {
-  al := alog.New(nil).SetOutput(os.Stderr).Do(ext.DoMode.TEST("test.log"))
+func TestExtDoMode(t *testing.T) {
+	al := alog.New(nil).SetOutput(os.Stderr).Do(ext.DoMode.TEST("test.log"))
 	tOS := al.NewTag("OS")
 	tSYS := al.NewTag("SYS")
 	al.Info(tOS).Str("status", "starting").Bool("isError", false).Write("starting")
