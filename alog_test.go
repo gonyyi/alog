58a947ed0871a4e4da83375a90a65e4ffdd0ff94
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-04 14:29:51 -0600

    Update alog_test.go

--- alog_test.go
+++ alog_test.go
@@ -23,15 +23,23 @@ func TestRandom(t *testing.T) {
 
 func TestNew(t *testing.T) {
 	al := alog.New(os.Stderr)
+	al.Control.Level = alog.Ltrace
 
 	tOS := al.NewTag("OS")
 	tSYS := al.NewTag("SYS")
 
 	test := func() {
+		al.Trace(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
+		al.Debug(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
 		al.Info(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
+		al.Warn(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
+		al.Error(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
+		al.Fatal(tOS|tSYS).Err("err1", nil).Err("err2", err1).Str("ok", "yes okay").Write("")
 	}
 
 	test()
-	al.SetFormatter(ext.NewFormatterTerminal())
+	al = al.SetFormatter(ext.NewFormatterTerminal())
+	test()
+	al = al.SetFormatter(ext.NewFormatterTerminalColor())
 	test()
 }
