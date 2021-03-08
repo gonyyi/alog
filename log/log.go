8ee43606afe6aa846b8302f97184745acdc99a71
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-08 11:27:10 -0600

    Update log.go

--- log/log.go
+++ log/log.go
@@ -20,26 +20,26 @@ func NewTag(name string) alog.Tag {
 	return al.NewTag(name)
 }
 
-func Trace(t alog.Tag) *alog.Entry {
-	return al.Trace(t)
+func Trace(t ...alog.Tag) *alog.Entry {
+	return al.Trace(t...)
 }
 
-func Debug(t alog.Tag) *alog.Entry {
-	return al.Debug(t)
+func Debug(t ...alog.Tag) *alog.Entry {
+	return al.Debug(t...)
 }
 
-func Info(t alog.Tag) *alog.Entry {
-	return al.Info(t)
+func Info(t ...alog.Tag) *alog.Entry {
+	return al.Info(t...)
 }
 
-func Warn(t alog.Tag) *alog.Entry {
-	return al.Warn(t)
+func Warn(t ...alog.Tag) *alog.Entry {
+	return al.Warn(t...)
 }
 
-func Error(t alog.Tag) *alog.Entry {
-	return al.Error(t)
+func Error(t ...alog.Tag) *alog.Entry {
+	return al.Error(t...)
 }
 
-func Fatal(t alog.Tag) *alog.Entry {
-	return al.Fatal(t)
+func Fatal(t ...alog.Tag) *alog.Entry {
+	return al.Fatal(t...)
 }
