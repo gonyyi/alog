74495e6a611f28f57db4d217ea1c97fead787c77
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-10 11:20:15 -0600

    benchmark sh update

--- s_benchmark.sh
+++ s_benchmark.sh
@@ -5,6 +5,12 @@ if [ -z "$1" ] ; then
   # if no args given, run bench on screen
   go test -bench=.
 else
-  go test -bench=. $1 > ./tmp/bench.txt
-  echo "Saved to ./tmp/bench.txt"
+  if [ -z "$2" ] ; then
+    go test -bench=. $1 > ./tmp/bench.json
+    echo "Saved to ./tmp/bench.json"
+  else
+    go test -bench=. $1 > ./tmp/$2
+    echo "Saved to ./tmp/$2"
+  fi
+
 fi
