f3e7cd7c6096109346f3031fff6c1976240ca66f
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-10 11:24:29 -0600

    rename scripts prefix from s_ to z_

--- s_inline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-go build -gcflags="-m -m" 2>&1 |grep "cannot" |sort > out.txt
-# go build -gcflags="-m -m" 2>&1 | sort > out.txt && sublime out.txt
-
-echo "\n-------\n" >> out.txt 
-
-go build -gcflags="-m -m" 2>&1 |grep "can inline" |sort >> out.txt && sublime out.txt
