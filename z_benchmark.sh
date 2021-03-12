5761227a4a7685c6047301abb7ff5c9b7a61328a
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-12 11:59:56 -0600

    Update z_benchmark.sh
    as json format turned out pretty useless..

--- z_benchmark.sh
+++ z_benchmark.sh
@@ -5,12 +5,6 @@ if [ -z "$1" ] ; then
   # if no args given, run bench on screen
   go test -bench=.
 else
-  if [ -z "$2" ] ; then
-    go test -bench=. $1 > ./tmp/bench.json
-    echo "Saved to ./tmp/bench.json"
-  else
-    go test -bench=. $1 > ./tmp/$2
-    echo "Saved to ./tmp/$2"
-  fi
-
+  go test -bench=. > ./tmp/$1
+  echo "Saved to ./tmp/$1"
 fi
