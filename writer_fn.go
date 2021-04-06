a1d34edab51280b1d956a788a29f2fafa83d0626
Gon <29622355+gonyyi@users.noreply.github.com>
2021-04-06 13:34:54 -0500

    Update writer_fn.go

--- writer_fn.go
+++ writer_fn.go
@@ -3,31 +3,31 @@ package alog
 type FnWrite func(b []byte, level Level, tag Tag) (int, error)
 type FnClose func() error
 
-type fnWriter struct {
+type writerFn struct {
 	fnWrite FnWrite
 	fnClose FnClose
 }
 
-func (w fnWriter) WriteLt(p []byte, level Level, tag Tag) (int, error) {
+func (w writerFn) WriteLt(p []byte, level Level, tag Tag) (int, error) {
 	return w.fnWrite(p, level, tag)
 }
 
-func (w fnWriter) Write(p []byte) (n int, err error) {
+func (w writerFn) Write(p []byte) (n int, err error) {
 	return w.fnWrite(p, 0, 0)
 }
 
-func (w fnWriter) Close() error {
+func (w writerFn) Close() error {
 	return w.fnClose()
 }
 
-func NewFnWriter(fnWrite FnWrite, fnClose FnClose) *fnWriter {
+func NewWriterFn(fnWrite FnWrite, fnClose FnClose) *writerFn {
 	if fnWrite == nil {
 		fnWrite = func(b []byte, level Level, tag Tag) (int, error) { return 0, nil }
 	}
 	if fnClose == nil {
 		fnClose = func() error { return nil }
 	}
-	return &fnWriter{
+	return &writerFn{
 		fnWrite: fnWrite,
 		fnClose: fnClose,
 	}
